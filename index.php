bbe86638c54d53200458eecc337ea2040e5111b1
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-08-04 19:55:50 +0900

    index.php styling

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-hello everyone!
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Noodle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+   <div class="wrapper indexPage">
+    <div class="mainSection">
+        <div class="logoContainer">
+
+            <img src="assets/images/festisite_google.png">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php" method="GET">
+                <input class="searchBox" type="text" name="term">
+                <input class="searchButton" type="submit" value="Search">
+            </form>
+
+        </div>
+        </div>
+    </div>
+        </body>
+
+        </html>
