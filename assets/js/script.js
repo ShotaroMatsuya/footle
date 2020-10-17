78ce4262c0c448b763c7e757d6af6cb4ee461186
ShotaroMatsuya <matsuya@h01.itscom.net>
2020-10-18 00:35:31 +0900

    adding youtube search & wikipedia search

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,3 +92,462 @@ function increaseImageClicks(imageUrl) {
     }
   });
 }
+
+
+$(function() {
+	$('#search-form').submit(function(e) {
+		e.preventDefault();
+	})
+});
+const APIkey = "";
+function search() {
+	//Clear any previous results 
+	$('#results').html('');
+	$('#btn-cnt').html('');	
+	
+	//Get form input
+	var q = $('#query').val();
+	
+	//Run GET Request 	on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+            type: 'video',
+            maxResults:20,
+			key: APIkey}, 
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				var pageInfo = data.pageInfo;
+				
+				//Log data
+				console.log(data);	
+				
+				$.each(data.items, function(i, item) {
+						//Get output
+						var output = getOutput(item);
+						
+						//Display results
+						$('#results').append(output);	
+				});
+				
+				var buttons = getButtons(prevPageToken,nextPageToken, pageInfo);
+				
+				//Display buttons
+				$('#results').prepend(buttons);
+				$('#btn-cnt').append(buttons);
+			}
+	);	
+}
+
+//Build Output
+	function getOutput(item) {
+		var videoId = item.id.videoId;
+		var title = item.snippet.title;
+		var description = item.snippet.description;
+		var thumb = item.snippet.thumbnails.high.url;
+		var channelTitle = item.snippet.channelTitle;
+		var videoDate = item.snippet.publishedAt;
+		
+		//Build Output String
+		var output = '<div class="row">' + 
+		'<div class="col-sm-2">' +	
+			'<a data-fancybox href="https://www.youtube.com/watch?v=' + videoId + '"><img class="img-fluid" src="' + thumb + '"></a>' +
+		'</div>' +
+		'<div class="col-sm-10">' + 
+			'<h4><a data-fancybox href="https://www.youtube.com/watch?v=' + videoId + '">' + title + '</a></h4>' +
+			'<small>By <span class="cTitle">' + channelTitle + '</span> on '+ videoDate + '</small>' +
+			'<p>' + description + '</p>' +
+		'</div>' +
+		'</div>';
+		return output;
+			
+	}
+
+//Build the Buttons
+	function getButtons(prevPageToken,nextPageToken,pageInfo) {
+		
+		$('#btn-cnt').html('');	
+		var btnoutput;
+		var q = $('#query').val();
+		if(!prevPageToken) {
+			btnoutput = '<div class="button-container">' + 
+			'<span class="total-results"><label>Total Results :</label>' + pageInfo.totalResults + '</span>' +
+			'<button id="next-button" class="btn animated-button thar-three" data-token="' + 	nextPageToken + '" data-query="' + q +'"' +
+			'onclick="nextPage();">Next Page</button><div class="clearfix"></div></div>';
+			console.log(nextPageToken);
+		} else {
+			console.log(nextPageToken);
+			btnoutput = '<div class="button-container">' +
+			'<span class="total-results"><label>Total Results :</label>' + pageInfo.totalResults + '</span>' +
+			'<button id="next-button" class="btn  animated-button thar-three" data-token="' + 	nextPageToken + '" data-query="' + q +'"' +
+			'onclick="nextPage();">Next Page</button>' +
+			'<button id="prev-button" class="btn  animated-button thar-four" data-token="' + 	prevPageToken + '" data-query="' + q +'"' +
+			'onclick="prevPage();">Previous Page</button>' +
+			'<div class="clearfix"></div></div>';
+		}
+		return btnoutput;
+	}
+
+function nextPage() {
+		
+		var token = $('#next-button').data('token');
+		var q = $('#next-button').data('query');
+		//Clear any previous results 
+	$('#results').html('');
+	$('#btn-cnt').html('');	
+	
+	//Get form input
+	q = $('#query').val();
+	
+	//Run GET Request 	on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+            type: 'video',
+            maxResults:20,
+			key: APIkey}, 
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				var pageInfo = data.pageInfo;
+				//Log data
+				console.log(data);	
+				
+				$.each(data.items, function(i, item) {
+						//Get output
+						var output = getOutput(item);
+						
+						//Display results
+						$('#results').append(output);	
+				});
+				
+				var buttons = getButtons(prevPageToken,nextPageToken,pageInfo);
+				
+				//Display buttons
+				$('#results').prepend(buttons);
+				$('#btn-cnt').append(buttons);
+			}
+		);
+			
+}
+
+function prevPage() {
+		
+		var token = $('#prev-button').data('token');
+		var q = $('#prev-button').data('query');
+		//Clear any previous results 
+	$('#results').html('');
+	$('#btn-cnt').html('');	
+	
+	//Get form input
+	q = $('#query').val();
+	
+	//Run GET Request 	on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+            type: 'video',
+            maxResults:20,
+			key: APIkey}, 
+			function(data) {
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				var pageInfo = data.pageInfo;
+				//Log data
+				console.log(data);	
+				
+				$.each(data.items, function(i, item) {
+						//Get output
+						var output = getOutput(item);
+						
+						//Display results
+						$('#results').append(output);	
+				});
+				
+				var buttons = getButtons(prevPageToken,nextPageToken,pageInfo);
+				
+				//Display buttons
+				$('#results').prepend(buttons);
+				$('#btn-cnt').append(buttons);
+			}
+		);
+			
+}
+
+// wiki
+const EnterKeyCode = 13;
+var input = document.getElementById('search'),
+  resultsContainer = document.getElementById('container-results'),
+  aboutResults = document.getElementById('about-results'),
+  globalData,
+  nextBtn = document.getElementById('next'),
+  previousBtn = document.getElementById('previous'),
+  totalhits = 0,
+  globalTotalhits,
+  currentPage = 1,
+  sroffset = 0,
+  searchBtn = document.getElementById('searchBtn');
+
+function resetValues() {
+  currentPage = 1;
+  sroffset = 0;
+  aboutResults.innerHTML = '';
+}
+
+function callback(data) {
+  globalData = data;
+}
+// /w/api.php?action=query&format=json&list=random&rnlimit=20
+nextBtn.addEventListener('click', nextBtnClickHandler);
+
+function nextBtnClickHandler(e) {
+  if (totalhits - 20 > 0) {
+    if (nextBtn.disabled) {
+      nextBtn.disabled = false;
+    }
+    clearOldResults();
+    sroffset += 20;
+    currentPage++;
+    JSONP(
+      'https://ja.wikipedia.org/w/api.php?action=query&list=search&srsearch=' +
+        encodeURIComponent(term) +
+        '&utf8=&format=json&srprop=score|size|snippet&sroffset=' +
+        sroffset +
+        '&callback=callback&srlimit=20'
+    );
+    whenAvailable('globalData', function () {
+      resultsContainer.style.display = 'inline-block';
+      aboutResults.innerHTML =
+        'found ' +
+        totalhits +
+        ' results ' +
+        'current page:' +
+        currentPage +
+        ' of ' +
+        Math.round(globalTotalhits / 20);
+      if (totalhits > 20) {
+        nextBtn.style.display = 'inline-block';
+        previousBtn.style.display = 'inline-block';
+      }
+      for (var i = 0; i < 20; i++) {
+        var result = document.createElement('div');
+        result.className = 'result';
+        // result.innerHTML = globalData.query.search[i].snippet;
+        var aTag = document.createElement('a');
+        aTag.href =
+          'https://ja.wikipedia.org/wiki/' +
+          globalData.query.search[i].title.replace(/\s/g, '_');
+        aTag.innerHTML = globalData.query.search[i].title;
+        result.appendChild(aTag);
+        var textNode = document.createTextNode(
+          ' ' + globalData.query.search[i].snippet.replace(/<.*>/g, '')
+        );
+        result.appendChild(textNode);
+        resultsContainer.appendChild(result);
+      }
+    });
+  } else {
+    nextBtn.disabled = true;
+  }
+}
+previousBtn.addEventListener('click', previousBtnClickHandler);
+
+function previousBtnClickHandler(e) {
+  if (currentPage > 1) {
+    // console.log("curret page:"+currentPage+" sroffset:"+sroffset);
+    sroffset -= 20;
+    // console.log("curret page:"+currentPage+" sroffset:"+sroffset);
+    currentPage--;
+    clearOldResults();
+    JSONP(
+      'https://ja.wikipedia.org/w/api.php?action=query&list=search&srsearch=' +
+        encodeURIComponent(term) +
+        '&utf8=&format=json&srprop=score|size|snippet&sroffset=' +
+        sroffset +
+        '&callback=callback&srlimit=20'
+    );
+    whenAvailable('globalData', function () {
+      resultsContainer.style.display = 'inline-block';
+      aboutResults.innerHTML =
+        'results ' +
+        'current page:' +
+        currentPage +
+        ' of ' +
+        Math.round(globalTotalhits / 20);
+      if (totalhits > 20) {
+        nextBtn.style.display = 'inline-block';
+        previousBtn.style.display = 'inline-block';
+      }
+      for (var i = 0; i < 20; i++) {
+        var result = document.createElement('div');
+        result.className = 'result';
+        // result.innerHTML = globalData.query.search[i].snippet;
+        var aTag = document.createElement('a');
+        aTag.href =
+          'https://ja.wikipedia.org/wiki/' +
+          globalData.query.search[i].title.replace(/\s/g, '_');
+        aTag.innerHTML = globalData.query.search[i].title;
+        result.appendChild(aTag);
+        var textNode = document.createTextNode(
+          ' ' + globalData.query.search[i].snippet.replace(/<.*>/g, '')
+        );
+        result.appendChild(textNode);
+        resultsContainer.appendChild(result);
+      }
+    });
+  }
+}
+input.addEventListener('input', inputInputHandler);
+
+function inputInputHandler(e) {
+  if (e.target.value === '') {
+    clearOldResults();
+    resetValues();
+    aboutResults.innerHTML = '';
+    next.style.display = 'none';
+    previous.style.display = 'none';
+  }
+}
+input.addEventListener('keyup', inputKeyupHandler);
+function inputKeyupHandler(e) {
+  resetValues();
+  if (e.keyCode === EnterKeyCode) {
+    clearOldResults();
+    term = e.target.value;
+    if (!term) {
+      clearOldResults();
+    }
+    resultsContainer.style.display = 'block';
+    console.log('first sroffset:' + sroffset);
+    JSONP(
+      'https://ja.wikipedia.org/w/api.php?action=query&list=search&srsearch=' +
+        encodeURIComponent(term) +
+        '&utf8=&format=json&srprop=score|size|snippet&sroffset=' +
+        sroffset +
+        '&callback=callback&srlimit=20'
+    );
+    whenAvailable('globalData', function (t) {
+      // do something
+      // console.log(globalData.query);
+      globalTotalhits = globalData.query.searchinfo.totalhits;
+      totalhits = globalTotalhits;
+      resultsContainer.style.display = 'inline-block';
+      aboutResults.innerHTML =
+        'found ' +
+        totalhits +
+        ' results ' +
+        'current page:' +
+        currentPage +
+        ' of ' +
+        Math.round(totalhits / 20);
+      if (totalhits > 20) {
+        nextBtn.style.display = 'inline-block';
+        previousBtn.style.display = 'inline-block';
+      }
+      for (var i = 0; i < 20; i++) {
+        var result = document.createElement('div');
+        result.className = 'result';
+        // result.innerHTML = globalData.query.search[i].snippet;
+        var aTag = document.createElement('a');
+        aTag.href =
+          'https://ja.wikipedia.org/wiki/' +
+          globalData.query.search[i].title.replace(' ', '_');
+        aTag.innerHTML = globalData.query.search[i].title;
+        result.appendChild(aTag);
+        var textNode = document.createTextNode(
+          ' ' + globalData.query.search[i].snippet.replace(/<.*>/g, '')
+        );
+        result.appendChild(textNode);
+        resultsContainer.appendChild(result);
+      }
+      globalData = 'undefined';
+    });
+  }
+}
+searchBtn.addEventListener('click', searchBtnClickHandler);
+function searchBtnClickHandler() {
+  resetValues();
+
+  clearOldResults();
+  term = input.value;
+  if (!term) {
+    clearOldResults();
+  }
+  resultsContainer.style.display = 'block';
+  console.log('first sroffset:' + sroffset);
+  JSONP(
+    'https://ja.wikipedia.org/w/api.php?action=query&list=search&srsearch=' +
+      encodeURIComponent(term) +
+      '&utf8=&format=json&srprop=score|size|snippet&sroffset=' +
+      sroffset +
+      '&callback=callback&srlimit=20'
+  );
+  whenAvailable('globalData', function (t) {
+    // do something
+    // console.log(globalData.query);
+    globalTotalhits = globalData.query.searchinfo.totalhits;
+    totalhits = globalTotalhits;
+    resultsContainer.style.display = 'inline-block';
+    aboutResults.innerHTML =
+      'found ' +
+      totalhits +
+      ' results ' +
+      'current page:' +
+      currentPage +
+      ' of ' +
+      Math.round(totalhits / 20);
+    if (totalhits > 20) {
+      nextBtn.style.display = 'inline-block';
+      previousBtn.style.display = 'inline-block';
+    }
+    for (var i = 0; i < 20; i++) {
+      var result = document.createElement('div');
+      result.className = 'result';
+      // result.innerHTML = globalData.query.search[i].snippet;
+      var aTag = document.createElement('a');
+      aTag.href =
+        'https://ja.wikipedia.org/wiki/' +
+        globalData.query.search[i].title.replace(' ', '_');
+      aTag.innerHTML = globalData.query.search[i].title;
+      result.appendChild(aTag);
+      var textNode = document.createTextNode(
+        ' ' + globalData.query.search[i].snippet.replace(/<.*>/g, '')
+      );
+      result.appendChild(textNode);
+      resultsContainer.appendChild(result);
+    }
+    globalData = 'undefined';
+  });
+}
+
+function JSONP(url) {
+  var scriptTag = document.createElement('script');
+  scriptTag.type = 'text/javascript';
+  scriptTag.src = url;
+  var appendedChild = document.head.appendChild(scriptTag);
+  appendedChild.remove();
+}
+
+function clearOldResults() {
+  aboutResults.innerHTML = '';
+  while (resultsContainer.firstChild) {
+    resultsContainer.removeChild(resultsContainer.firstChild);
+  }
+}
+
+function whenAvailable(name, callback) {
+  var interval = 10; // ms
+  window.setTimeout(function () {
+    if (typeof window[name] === 'object') {
+      callback(window[name]);
+    } else {
+      window.setTimeout(arguments.callee, interval);
+    }
+  }, interval);
+}
--- search.php
+++ search.php
@@ -3,6 +3,8 @@ require_once("config.php");
 require_once("classes/SiteResultsProvider.php");
 require_once("classes/ImageResultsProvider.php");
 
+
+
 if (isset($_GET["term"])) {
     $term = $_GET["term"];
 } else {
@@ -20,6 +22,7 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
     <meta charset="UTF-8">
     <title>Welcome to Doodle</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </head>
@@ -52,6 +55,12 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
                     <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
                         <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a>
                     </li>
+                    <li class="<?php echo $type == 'movies' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=movies"; ?>'>Movies</a>
+                    </li>
+                    <li class="<?php echo $type == 'wiki' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=wiki"; ?>'>Wikipedia</a>
+                    </li>
                 </ul>
 
             </div>
@@ -65,75 +74,136 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
             if ($type == "sites") {
                 $resultsProvider = new SiteResultsProviders($con);
                 $pageSize = 20;
-            } else {
+                $numResults = $resultsProvider->getNumResults($term);
+
+                echo "<p class='resultsCount'>$numResults results found</p>";
+
+                echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
+            } elseif ($type == "images") {
                 $resultsProvider = new ImageResultsProviders($con);
                 $pageSize = 30;
+                $numResults = $resultsProvider->getNumResults($term);
+
+                echo "<p class='resultsCount'>$numResults results found</p>";
+
+                echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
+            } elseif ($type == "movies") {
+                echo '<nav class="navbar navbar-light bg-faded">
+                <div class="row">
+                     <div class="col-sm-3">
+                          <h1 class="logo" ><i class="fa fa-youtube yt"></i><span>Vidz</span></h1>
+                     </div>
+                    <div class="col-sm-9">
+                        
+                         <form class="form-inline " style="margin-top:5px;" id="search-form" name="search-form" onSubmit="return search()">
+                                 <table width="100%">
+                                    <tr>
+                                        <td width="100%">
+                                            <div class="d3 float-right" style="width:100%;" >	
+                                              <input class="form-control search-field" type="search" id="query" style="width:100%;"  placeholder="Search Youtube">
+                                              <span class="focus-border">
+                                                        <i></i>
+                                                    </span>
+                                                </div>
+                                        </td>
+                                        <td>
+                                              <button class="btn btn-outline-danger my-2 my-sm-0 " type="submit" id="search-btn"  name="search-btn" value="">Search</button>
+                                        </td>
+                                </tr>                
+                              </table>  
+                            </form>
+                    </div>
+                </div>
+             
+            </nav>
+            <div class="content">
+            <div class="container-fluid" id="results">
+            </div> <!-- ENd of container fluid -->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">    <div id="btn-cnt"></div> </div>
+                </div>
+            </div>
+            </div> <!-- End of content -->';
+            } elseif ($type == "wiki") {
+                echo '<div class="first-row">
+                <input
+                  id="search"
+                  name="Search"
+                  type="search"
+                  placeholder="Search"
+                /><input id="searchBtn" type="button" value="Enter" />
+              </div>
+              <div id="about-results" class=""></div>
+              <div id="container-results" class=""></div>
+              <br />
+              <input id="next" type="button" value="next" />
+              <input id="previous" type="button" value="previous" />';
             }
 
-            $numResults = $resultsProvider->getNumResults($term);
-
-            echo "<p class='resultsCount'>$numResults results found</p>";
-
-            echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
             ?>
 
         </div>
         <div class="paginationContainer">
-            <div class="pageButtons">
-                <div class="pageNumberContainer">
-                    <img src="assets/images/pageStart.png">
-                </div>
-                <?php
-                $pagesToShow = 10; /* oの表示数のmax */
-                $numPages = ceil($numResults / $pageSize); /*小数点切り上げ 総ページ数*/
-                $pagesLeft = min($pagesToShow, $numPages); /*min関数は最小値の方を返す ループで回すoの数*/
+            <?php if ($type == "sites" || $type == "images") { ?>
+                <div class="pageButtons">
+                    <div class="pageNumberContainer">
+                        <img src="assets/images/pageStart.png">
+                    </div>
+                    <?php
+                    $pagesToShow = 10; /* oの表示数のmax */
+                    $numPages = ceil($numResults / $pageSize); /*小数点切り上げ 総ページ数*/
+                    $pagesLeft = min($pagesToShow, $numPages); /*min関数は最小値の方を返す ループで回すoの数*/
 
-                // ループ開始地点の設定
-                $currentPage = $page - floor($pagesToShow / 2); /*小数点切り下げ 　ループ開始地点*/
-                if ($currentPage < 1) {  /*ページ数が5より少ないときのループ開始地点は1から */
-                    $currentPage = 1;
-                }
-                if ($currentPage + $pagesLeft > $numPages + 1) {  /* ページmax付近でのループ開始地点 */
-                    $currentPage = $numPages + 1 - $pagesLeft;
-                }
+                    // ループ開始地点の設定
+                    $currentPage = $page - floor($pagesToShow / 2); /*小数点切り下げ 　ループ開始地点*/
+                    if ($currentPage < 1) {  /*ページ数が5より少ないときのループ開始地点は1から */
+                        $currentPage = 1;
+                    }
+                    if ($currentPage + $pagesLeft > $numPages + 1) {  /* ページmax付近でのループ開始地点 */
+                        $currentPage = $numPages + 1 - $pagesLeft;
+                    }
 
-                while ($pagesLeft != 0 && $currentPage <= $numPages) { /*currentPageを増やし、pagesLeftをへらす */
-                    if ($currentPage == $page) {
-                        echo "<div class='pageNumberContainer'>
+                    while ($pagesLeft != 0 && $currentPage <= $numPages) { /*currentPageを増やし、pagesLeftをへらす */
+                        if ($currentPage == $page) {
+                            echo "<div class='pageNumberContainer'>
                                 <img src='assets/images/pageSelected.png'>
                                 <span class='pageNumber'>$currentPage</span>
     
                             </div>";
-                    } else {
-                        echo "<div class='pageNumberContainer'>
+                        } else {
+                            echo "<div class='pageNumberContainer'>
                                 <a href='search.php?term=$term&type=$type&page=$currentPage'>
                                     <img src='assets/images/page.png'>
                                     <span class='pageNumber'>$currentPage</span>
                                 </a>
                             </div>";
-                    }
-                    $currentPage++;
-                    $pagesLeft--;
-                }
+                        }
+                        $currentPage++;
+                        $pagesLeft--;
+                    } ?>
+                    <div class="pageNumberContainer">
+                        <img src="assets/images/pageEnd.png">
+                    </div>
 
 
-                ?>
-
-
-
-
-                <div class="pageNumberContainer">
-                    <img src="assets/images/pageEnd.png">
                 </div>
 
+            <?php  } elseif ($type == "movies") { ?>
 
-            </div>
-
-
+                paginationLink on youtube
+            <?php } elseif ($type == "wiki") { ?>
+                paginationLink on wikipedia
+            <?php } ?>
         </div>
 
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js">
+    </script>
 
     <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
     <script type="text/javascript" src="assets/js/script.js"></script>
