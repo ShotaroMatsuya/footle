d1640b90bb46528d90c658cf61716bfd05f3fed4
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-22 00:26:24 +0900

    FIX: toomanyrequest solution

--- aws/dockerfile.php
+++ aws/dockerfile.php
@@ -1,10 +1,10 @@
-FROM public.ecr.aws/docker/library/php:7.4-fpm-alpine
+FROM php:7.4-fpm-alpine
 
 WORKDIR /var/www/html
 
 COPY ./app/composer.json ./
 
-COPY --from=public.ecr.aws/composer/composer:2 /usr/bin/composer /usr/bin/composer
+COPY --from=composer:2 /usr/bin/composer /usr/bin/composer
 
 RUN composer install --ignore-platform-reqs
 # 設定ファイルを指定の場所に置く
