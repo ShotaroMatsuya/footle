1a6d7309b06a8703c037440bdc7fdb9e2d860dd7
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-23 12:18:26 +0900

    FIX: rename env to .env

--- aws/dockerfile.php
+++ aws/dockerfile.php
@@ -14,7 +14,7 @@ RUN docker-php-ext-install pdo pdo_mysql
 
 WORKDIR /var/www/html
 # .envをリネーム
-COPY ./app/.env.default ./env
+COPY ./app/.env.default ./.env
 
 COPY --from=composer_build /usr/bin/composer /usr/bin/composer
 COPY --chown=www-data --from=composer_build /app/ ./
