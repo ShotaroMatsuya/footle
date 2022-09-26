365f0e7e88d6f6886bc66de4f5221f753c720fd9
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-09-27 01:09:49 +0900

    Create cronjob.sh

---
+++ app/cronjob.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ジョブ1
+/usr/local/php/7.4/bin/php job1.php https://onefootball.com/en/home
+
+# ジョブ2
+/usr/local/php/7.4/bin/php job1.php https://onefootball.com/en/hom
+
+# ジョブ3
+/usr/local/php/7.4/bin/php job1.php https://www.bbc.com/sport/football
+
+# ジョブ4
+/usr/local/php/7.4/bin/php job1.php https://www.skysports.com/football/news
+
+# ジョブ5
+/usr/local/php/7.4/bin/php job1.php https://talksport.com/football/
+
+# ジョブ6
+/usr/local/php/7.4/bin/php job1.php https://www.transfermarkt.com/
